8ae718d285a4328f9c67264c932f7865debca2f0
amund <s.amund@icloud.com>
2023-09-27 20:17:38 +0200

    Started working on insert static data.

--- src/database/postgres.rs
+++ src/database/postgres.rs
@@ -1,5 +1,7 @@
 use crate::live_ais::response_structs::{AISStaticData};
 use chrono::{DateTime, Utc};
+use rayon::iter::ParallelIterator;
+use rayon::prelude::IntoParallelRefIterator;
 use sqlx::types::Uuid;
 use sqlx::{query, Error, PgPool};
 
@@ -35,8 +37,26 @@ impl BarentsPostgresConnection {
         Ok(id)
     }
 
-    pub async fn insert_static_data(&self, static_data: AISStaticData) {
-        todo!();
+    pub async fn insert_static_data(&self, static_data: Vec<AISStaticData>, log_id: Uuid) -> Result<(), Box<dyn std::error::Error>> {
+        let pool = PgPool::connect(&self.connection_string).await?;
+        let tx = pool.begin().await?;
+
+        for data in static_data {
+            sqlx::query!(
+                "INSERT INTO ais.ais_static_data (
+                    type_field, message_type, mmsi, msgtime, imo_number, call_sign, destination, eta, name, draught,
+                    ship_length, ship_width, ship_type, dimension_a, dimension_b, dimension_c, dimension_d,
+                    position_fixing_device_type, report_class, log_id
+                ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20)",
+                data.type_field, data.message_type, data.mmsi, data.msgtime, data.imo_number, data.call_sign,
+                data.destination, data.eta, data.name, data.draught, data.ship_length, data.ship_width,
+                data.ship_type, data.dimension_a, data.dimension_b, data.dimension_c, data.dimension_d,
+                data.position_fixing_device_type, data.report_class, log_id
+            ).execute(&pool).await.unwrap();
+        }
+        tx.commit().await?;
+        pool.close().await;
+        Ok(())
     }
 }
 
